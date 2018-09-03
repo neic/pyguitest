c34f6e234a35ea72eec10dc09140d1405d3ef028
Mathias Dannesbo <neic@neic.dk>
2018-09-03 17:35:03 +0200

    Commit to test jenkins 2

--- main.py
+++ main.py
@@ -3,6 +3,6 @@ from PySide2.QtWidgets import QApplication, QLabel
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    label = QLabel("Hello Universe")
+    label = QLabel("Hello Local Super Cluster")
     label.show()
     sys.exit(app.exec_())
