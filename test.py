b7bac2c96546f94455a0879e2c2925b6315af3ae
Mathias Dannesbo <neic@neic.dk>
2018-09-03 20:43:49 +0200

    Fix failing testcase

--- test.py
+++ test.py
@@ -6,8 +6,8 @@ class TestMyFun(unittest.TestCase):
     def test_HelloWorld(self):
         self.assertEqual(MyFun(), "Hello World")
 
-    def test_Fail(self):
-        self.assertFalse(True)
+    def test_False(self):
+        self.assertFalse(False)
 
 if __name__ == '__main__':
     unittest.main()
