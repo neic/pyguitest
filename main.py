26040d53a8582bc2f485c114abbee31737eb013a
Mathias Dannesbo <neic@neic.dk>
2018-09-03 17:27:25 +0200

    Commit to test jenkins 1

--- main.py
+++ main.py
@@ -3,6 +3,6 @@ from PySide2.QtWidgets import QApplication, QLabel
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    label = QLabel("Hello World")
+    label = QLabel("Hello Universe")
     label.show()
     sys.exit(app.exec_())
