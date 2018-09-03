fe470307c48695f993e7524c85302ce5394235e4
Mathias Dannesbo <neic@neic.dk>
2018-09-03 20:41:32 +0200

    Add failing test

--- test.py
+++ test.py
@@ -6,6 +6,8 @@ class TestMyFun(unittest.TestCase):
     def test_HelloWorld(self):
         self.assertEqual(MyFun(), "Hello World")
 
+    def test_Fail(self):
+        self.assertFalse(True)
 
 if __name__ == '__main__':
     unittest.main()
